fa450c8fc26a257aceafc8448b2b9cb872c07738
OrangeSheepCool <chenyangshuang@qutke.com>
2023-03-05 15:08:39 +0800

    fix(middlewares): check input while sign up

--- src/middlewares/hash.middleware.ts
+++ src/middlewares/hash.middleware.ts
@@ -7,15 +7,23 @@ import { genSaltSync, hashSync } from 'bcrypt';
 import { NextFunction, Request, Response } from 'express';
 
 import { EXCEPTION_MSG } from '@/constants';
+import { validateEmail, validatePhone } from '@/utils';
 
 const saltRounds = 10;
 
 @Injectable()
 export class HashMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const credential: string | undefined = req.body.credential;
-    if (!credential)
-      return next(new BadRequestException(EXCEPTION_MSG.INPUT_INVALID));
+    const { identity, credential }: { identity?: string; credential?: string } =
+      req.body;
+    const inputInvalidException: BadRequestException = new BadRequestException(
+      EXCEPTION_MSG.INPUT_INVALID,
+    );
+
+    if (!identity) return next(inputInvalidException);
+    if (!validateEmail(identity) && !validatePhone(identity))
+      return next(inputInvalidException);
+    if (!credential) return next(inputInvalidException);
 
     const salt: string = genSaltSync(saltRounds);
     const hashed: string = hashSync(credential, salt);
--- src/utils/determine.util.ts
+++ src/utils/determine.util.ts
@@ -4,11 +4,11 @@ import { validateEmail, validatePhone } from './regex.util';
 export const determineIdentityName = (identity: string): string => {
   if (validateEmail(identity)) return '邮箱';
   if (validatePhone(identity)) return '手机号';
-  return '';
+  return '' as never;
 };
 
 export const determineIdentityEnum = (identity: string): AuthIdentifier => {
   if (validateEmail(identity)) return AuthIdentifier.EMAIL;
   if (validatePhone(identity)) return AuthIdentifier.PHONE;
-  return '' as AuthIdentifier;
+  return '' as never;
 };
