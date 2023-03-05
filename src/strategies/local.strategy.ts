4834557b2d884ea17fdd8b2773e4bf351f240d8e
OrangeSheepCool <chenyangshuang@qutke.com>
2023-03-05 14:34:03 +0800

    fix(strategies): local strategy fields

--- src/strategies/local.strategy.ts
+++ src/strategies/local.strategy.ts
@@ -13,18 +13,21 @@ const { ACCOUNT_NOT_FOUND, ACCOUNT_NOT_MATCHED } = EXCEPTION_MSG;
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
-    super();
+    super({
+      usernameField: 'identity',
+      passwordField: 'credential',
+    });
   }
 
-  async validate(username: string, password: string): Promise<User> {
-    const auth: Auth | null = await this.authService.findOne(username);
+  async validate(identity: string, credential: string): Promise<User> {
+    const auth: Auth | null = await this.authService.findOne(identity);
 
     if (!auth)
       throw new UnauthorizedException(
-        ACCOUNT_NOT_FOUND(determineIdentityName(username)),
+        ACCOUNT_NOT_FOUND(determineIdentityName(identity)),
       );
 
-    if (!compareSync(password, auth.credential))
+    if (!compareSync(credential, auth.credential))
       throw new UnauthorizedException(ACCOUNT_NOT_MATCHED);
 
     return auth.user;
